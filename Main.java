01a05a221c66b91a1cdeadb5998e1327f301febd
pguerreroarenas <92553422+pguerreroarenas@users.noreply.github.com>
2022-02-02 17:38:53 -0500

    Explicacion 1
    Aprendiendo a subir correctamente un repositorio cantera 2

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public static void main(String[] args) {
+        String name ="Julian";
+        System.out.println(name);
+        
+    }
+}
